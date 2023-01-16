fea979a4edace1e811d79ca674560b1e0905b327
np1961 <122818876+np1961@users.noreply.github.com>
2023-01-16 13:57:08 -0500

    Add files via upload

---
+++ key_locker.py
@@ -0,0 +1,7 @@
+    def loading_aplication(self):
+        loading=1
+        load='#########'
+        while(loading<100):
+            [print('___________________________________',loading,'%') for procedures in range(400000)]
+            [print(load,'Complate_typing') for index in range(loading)]
+            loading+=1
